574ad87335d8f8041333123f71d2f7a27916e3ab
DiegoV-20 <viloriego.05@gmail.com>
2026-03-22 12:51:54 -0400

    Configurando CORS en next.config.mjs

--- ianet-panel-adm/next.config.mjs
+++ ianet-panel-adm/next.config.mjs
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
+  async headers() {
+    return [
+      {
+        // Esto permite CORS en todas tus rutas de API
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://ianet-sistema2.vercel.app" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ]
   },
 }
 
-export default nextConfig
+module.exports = nextConfig
